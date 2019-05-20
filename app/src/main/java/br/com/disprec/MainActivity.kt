bfde2448e07c63a85f788e775f37e405b9c9ab54
Willans Junior <willans.junior@mv.com.br>
2019-05-20 14:07:27 -0300

    Resulmo de despesa.

--- app/src/main/java/br/com/disprec/MainActivity.kt
+++ app/src/main/java/br/com/disprec/MainActivity.kt
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import br.com.disprec.adapter.ListaTransacoesAdapter
 import br.com.disprec.model.Transacao
 import br.com.disprec.model.enums.TipoTransacao
+import br.com.disprec.util.extension.formatCurrent
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +18,28 @@ class MainActivity : AppCompatActivity() {
 
         val list = gerarListaTransacoes()
 
+        montarResumo(list)
+
         val adapter = ListaTransacoesAdapter(list, this)
 
         lv_transacoes.adapter = adapter
+    }
+
+    private fun montarResumo(list : List<Transacao>) {
+        var receita = BigDecimal.ZERO
+        var despesa = BigDecimal.ZERO
+        var media = BigDecimal.ZERO
+        for (transacao in list) {
+            if (transacao.tipo == TipoTransacao.RECEITA) {
+                receita = receita.plus(transacao.valor)
+            } else if (transacao.tipo == TipoTransacao.DESPESA) {
+                despesa = despesa.plus(transacao.valor)
+            }
+        }
+        resumo_card_receita.text = receita.formatCurrent(this)
+        resumo_card_despesa.text = despesa.formatCurrent(this)
+        var total = receita - despesa
+        resumo_card_total.text = total.formatCurrent(this)
     }
 
     private fun gerarListaTransacoes(): List<Transacao> {
